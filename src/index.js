94de2bf1b9b2f90f744c623a078b9f1f13523580
godhand1607 <jamcando@gmail.com>
2017-07-02 23:41:04 +0800

    Stash

--- src/index.js
+++ src/index.js
@@ -2,14 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { ReduxPromise } from 'redux-promise';
 
-import App from './components/app';
 import reducers from './reducers';
+import PostsIndex from './components/PostsIndex';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    <Provider store={createStoreWithMiddleware(reducers)}>
+        <BrowserRouter>
+            <div>
+                <Route path="/" component={PostsIndex}></Route>
+            </div>
+        </BrowserRouter>
+    </Provider>
+    , document.querySelector('.container'));
